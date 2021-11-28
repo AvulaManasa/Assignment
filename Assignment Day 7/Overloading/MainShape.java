ff7fe7aaea041936da9cfa92e64a67276265cd49
AvulaManasa <94954810+AvulaManasa@users.noreply.github.com>
2021-11-28 13:59:05 +0530

    Add files via upload

---
+++ Assignment Day 7/Overloading/MainShape.java
@@ -0,0 +1,15 @@
+package com.manasa.demo;
+
+public class MainShape {
+
+	public static void main(String[] args) {
+		Shape shape=new Shape();
+
+		shape.area(7, 5);
+		shape.area(6);
+		shape. periemeter(8);
+		shape. periemeter(2,4);
+		
+	}
+
+}
